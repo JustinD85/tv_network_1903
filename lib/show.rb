778913460df470a741f86564f50e97ecbde4b87c
Justin <justin@baal-software.com>
2019-03-27 09:19:37 -0600

    Create show class and test

---
+++ lib/show.rb
@@ -0,0 +1,16 @@
+require './lib/character'
+
+class Show
+  attr_reader :name, :creator, :characters
+
+  def initialize(name, creator, characters)
+    @name = name
+    @creator = creator
+    @characters = characters
+  end
+
+  def total_salary
+    @characters.sum { |char| char.salary }
+  end
+
+end
---
+++ test/show_test.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/character'
+require './lib/show'
+
+class ShowTest < MiniTest::Test
+  attr_reader :knight_rider, :kitt, :michael_knight
+
+  def setup
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+  end
+
+  def test_it_should_exist
+    assert_instance_of Show, knight_rider
+  end
+
+  def test_it_should_have_default_values
+    assert_equal "Knight Rider", knight_rider.name
+    assert_equal "Glen Larson", knight_rider.creator
+    assert_equal [michael_knight, kitt], knight_rider.characters
+    assert_equal 2600000, knight_rider.total_salary
+  end
+end
